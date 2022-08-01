a9b243c73b6435e4795a683522ff0d876b77aa95
Yogesh Bhattarai <bhattaraiyogesh007@gmail.com>
2022-08-01 17:39:29 +0545

    Updated the defaults

--- src/UI/Buttons.js
+++ src/UI/Buttons.js
@@ -1,14 +1,32 @@
 import styled from "@emotion/styled";
 import React from "react";
 
-const Buttons = ({ primary, secondary }) => {
-  return <Primary />;
+const Buttons = (props) => {
+  const { children, primary, secondary } = props;
+  if (primary) return <Primary {...props}>{children}</Primary>;
+  else if (secondary) return <Secondary {...props}>{children}</Secondary>;
+  else return <Default {...props}>{children}</Default>;
 };
 
 const Primary = styled.button({
   color: "#f8f8f8",
   background: "#2b2b2b",
+  border: "1px solid transparent",
   borderRadius: "0.3rem",
+});
+
+const Secondary = styled.button({
+  color: "#f8f8f8",
+  background: "#2b2b2b",
+  borderRadius: "0.3rem",
+  border: "1px solid #2b2b2b",
+});
+
+const Default = styled.button({
+  color: "#f8f8f8",
+  background: "transparent",
+  borderRadius: "0.3rem",
+  border: "1px solid transparent",
 });
 
 export default Buttons;
--- src/UI/Input.js
+++ src/UI/Input.js
@@ -1,14 +1,32 @@
 import styled from "@emotion/styled";
 import React from "react";
 
-const Input = ({ primary, secondary }) => {
-  return <Primary />;
+const Input = (props) => {
+  const { primary, secondary } = props;
+  if (primary) return <Primary {...props} />;
+  else if (secondary) return <Secondary {...props} />;
+  else return <Default {...props} />;
 };
 
 const Primary = styled.input({
   color: "#2b2b2b",
   borderRadius: "0.3rem",
   padding: "1rem",
+  border: "1px solid #2b2b2b",
+});
+
+const Default = styled.input({
+  color: "#2b2b2b",
+  borderRadius: "0.3rem",
+  padding: "1rem",
+  border: "1px solid #2b2b2b",
+});
+
+const Secondary = styled.input({
+  color: "#2b2b2b",
+  borderRadius: "0.3rem",
+  padding: "1rem",
+  border: "1px solid #2b2b2b",
 });
 
 export default Input;
