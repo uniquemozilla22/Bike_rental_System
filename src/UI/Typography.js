c7fb5fc2d60bdadc4e5df1fff61f6302b2539031
Yogesh Bhattarai <bhattaraiyogesh007@gmail.com>
2022-08-01 18:39:09 +0545

    Typography created for the application

---
+++ src/UI/Typography.js
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const Headings = styled.h1({
+  fontSize: "1.5rem",
+  fontWeight: "800",
+});
+
+export const SubHeadings = styled.h3({
+  fontSize: "1.2rem",
+  fontWeight: "600",
+});
+
+export const Paragraph = styled.p({
+  fontSize: "1rem",
+  fontWeight: "400",
+  color: "#2b2b2b50",
+});
